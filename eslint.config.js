32ddfa36596e50eec4f53999919e453dab68cc39
DanielAtCosmicDNA <daniel@cosmicdna.co.uk>
2025-10-29 13:07:47 +0000

    :alembic: Test with neostandard.

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,39 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 import neostandard, { plugins } from 'neostandard'
+import { globalIgnores } from 'eslint/config'
 
 export default [
+  globalIgnores(['dist']),
   ...neostandard(),
+  js.configs.recommended,
   plugins.n.configs['flat/recommended'],
+  plugins.react.configs.flat.recommended,
+  {
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-refresh/only-export-components': 'warn'
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  }
 ]
